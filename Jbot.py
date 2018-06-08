809cf2286fb8446ac8bb2bd14587a9d2bf8ff0d8
John <Sp4zzy@users.noreply.github.com>
2018-06-08 16:45:11 -0700

    Updated !clear command to ignore pinned messages

--- Jbot.py
+++ Jbot.py
@@ -3,6 +3,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
+import datetime
 
 Client = discord.Client()
 client = commands.Bot(command_prefix = "!")
@@ -10,6 +11,12 @@ client = commands.Bot(command_prefix = "!")
 @client.event
 async def on_ready():
     print("Jbot is ready!")
+
+def is_pinned(message):
+    if message.pinned:
+        return False
+    else:
+        return True
 
 @client.event
 async def on_message(message):
@@ -27,12 +34,27 @@ async def on_message(message):
                 msg = []
                 async for i in client.logs_from(message.channel, limit = number+1):
                     msg.append(i)
-                await client.delete_messages(msg)
+                await client.purge_from(message.channel, limit = number+1, check = is_pinned)#after = datetime.date(2018,6,1))
             else:
                 await client.send_message(message.channel, "Oops, you forgot to tell me how many to delete!")
         else:
             await client.send_message(message.channel, "You're not a Supreme Commander...")
 
 
-client.run("INPUT YOUR OWN SHIT HERE")
+
+##@client.command(pass_context = True)
+##async def purge(number):
+##    if "400365260990578691" in [role.id for role in message.author.roles]:
+##        number = int(number)
+##        counter = 0
+##        async for i in client.logs_from(message.channel, limit = number):
+##            if counter < number:
+##                await client.delete_message(i)
+##                counter += 1
+##                await asyncio.sleep(1.2)
+##    else:
+##        await client.send_message(message.channel, "You're not a Supreme Commander...")
+
+
+client.run("NDU0NzMwMTY2ODE2NjA0MTgw.DfyMsw.KTw0c8mrlyWdrFOq2zVYKDkceKI")
 
